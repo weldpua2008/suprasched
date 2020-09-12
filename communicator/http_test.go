8ec4a0c592efe9f0c9b7909eef4883dc5f960e83
Valeriy Solovyov <weldpua2008@gmail.com>
2020-09-12 08:18:41 +0300

    [tests] Initial for RestCommunicator

--- communicator/http_test.go
+++ communicator/http_test.go
@@ -1,123 +1,146 @@
 package communicator
 import (
 	// "bytes"
-	// "context"
-	// "encoding/json"
-	// "fmt"
+    "errors"
+	"context"
+	"encoding/json"
+	"fmt"
 	// "github.com/spf13/viper"
-	// "io/ioutil"
-	// "net/http"
-	// "net/http/httptest"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	// "time"
     config "github.com/weldpua2008/suprasched/config"
 
 )
+type Response struct {
+    Number int    `json:"number"`
+    Str  string `json:"str"`
+}
+
 
 func TestFetch(t *testing.T) {
     config.LoadCfgForTests(t, "fixtures/fetch_http.yml")
 
-	// // startTrace()
 	// want := "{\"job_uid\":\"job-testing.(*common).Name-fm\",\"run_uid\":\"1\",\"extra_run_id\":\"1\",\"msg\":\"'S'\\n\"}"
-	// var got string
-	// CMD := "echo && exit 0"
-	// responses := []ApiJobResponse{
-	// 	{
-	// 		JobId:       "job_id",
-	// 		JobStatus:   "PENDING",
-	// 		JobName:     "job_name",
-	// 		RunUID:      "run_uid",
-	// 		ExtraRunUID: "extra_run_id",
-	// 		CMD:         CMD,
-	// 		Parameters:  []string{},
-	// 		CreateDate:  "createDate",
-	// 		LastUpdated: "lastUpdated",
-	// 		StopDate:    "stopDate",
-	// 	},
-	// 	{
-	// 		JobId:       "job_id",
-	// 		JobStatus:   "PENDING",
-	// 		JobName:     "job_name",
-	// 		RunUID:      "run_uid",
-	// 		ExtraRunUID: "extra_run_id",
-	// 		CMD:         CMD,
-	// 		Parameters:  []string{},
-	// 		CreateDate:  "createDate",
-	// 		LastUpdated: "lastUpdated",
-	// 		StopDate:    "stopDate",
-	// 	},
-	// }
-    //
-	// // notifyStdoutSent := make(chan bool)
-	// srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    //
-	// 	var c ApiJobResponse
-	// 	if len(responses) > 1 {
-	// 		c, responses = responses[0], responses[1:]
-	// 	} else if len(responses) == 1 {
-	// 		c = responses[0]
-	// 	}
-	// 	c1 := make([]ApiJobResponse, 0)
-	// 	c1 = append(c1, c)
-	// 	js, err := json.Marshal(&c1)
-	// 	if err != nil {
-	// 		log.Tracef("Failed to marshal for '%v' due %v", c, err)
-	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 		return
-	// 	}
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	if _, errWrite := w.Write(js); errWrite != nil {
-	// 		t.Errorf("Can't w.Write %v due %v\n", js, err)
-	// 	}
-	// 	b, err := ioutil.ReadAll(r.Body)
-	// 	if err != nil {
-	// 		t.Errorf("ReadAll %s", err)
-	// 	}
-	// 	got = string(fmt.Sprintf("%s", b))
-	// 	// notifyStdoutSent <- true
-	// }))
-	// defer func() {
-	// 	srv.Close()
-	// 	model.FetchNewJobAPIURL = ""
-	// 	// restoreLevel()
-	// }()
-	// viper.SetConfigType("yaml")
-	// var yamlExample = []byte(`
-    // logs:
-    //   update:
-    //     method: GET
-    // jobs:
-    //   get:
-    //     url: "` + srv.URL + `"
-    //     method: POST
-    // `)
-    //
-	// _ = viper.ReadConfig(bytes.NewBuffer(yamlExample))
-    //
-	// model.FetchNewJobAPIURL = srv.URL
-	// log.Trace(fmt.Sprintf("model.FetchNewJobAPIURL  %s", model.FetchNewJobAPIURL))
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel() // cancel when we are getting the kill signal or exit
-	// jobs := make(chan *model.Job, 1)
-    //
-	// go func() {
-	// 	if err := StartGenerateJobs(ctx, jobs, time.Duration(5)*time.Second); err != nil {
-	// 		log.Infof("StartGenerateJobs failed %v", err)
-	// 	}
-	// }()
-	// for job := range jobs {
-	// 	if job.Status != model.JOB_STATUS_PENDING {
-	// 		t.Errorf("Expected %s, got %s", model.JOB_STATUS_PENDING, job.Status)
-	// 	}
-    //
-	// 	if job.CMD != CMD {
-	// 		t.Errorf("want %s, got %v", want, got)
-	// 	}
-	// 	job.Status = model.JOB_STATUS_CANCELED
-	// 	// stop loop
-	// 	if len(responses) == 1 {
-	// 		cancel()
-	// 	}
-    //
-	// }
+	var globalGot string
+	responses := []Response{
+		{
+			Number:       1,
+			Str:   "Str",
+		},
+		{
+            Number:       2,
+			Str:   "Str1",
+		},
+	}
+    // Response server.
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var c Response
+		if len(responses) > 1 {
+			c, responses = responses[0], responses[1:]
+		} else if len(responses) == 1 {
+			c = responses[0]
+		}
+		c1 := make([]Response, 0)
+		c1 = append(c1, c)
+		js, err := json.Marshal(&c1)
+		if err != nil {
+			log.Tracef("Failed to marshal for '%v' due %v", c, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		if _, errWrite := w.Write(js); errWrite != nil {
+			t.Errorf("Can't w.Write %v due %v\n", js, err)
+		}
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("ReadAll %s", err)
+		}
+		globalGot = string(fmt.Sprintf("%s", b))
+		// notifyStdoutSent <- true
+	}))
+	defer func() {
+		srv.Close()
+	}()
+
+    cases := []struct {
+        section   string
+        params map[string]interface{}
+        want map[string]interface{}
+        wantErr error
+        wantFetchErr error
+    }{
+        {
+            section:   "get",
+            params: map[string]interface{}{
+                "method": "GET",
+                "url": srv.URL,
+            },
+            want: map[string]interface{}{
+                "Number": responses[0].Number,
+                "Str": responses[0].Str,
+            },
+            wantErr: nil,
+            wantFetchErr: nil,
+        },
+        {
+            section:   "get",
+            params: map[string]interface{}{
+                "method": "GET",
+                "url": srv.URL,
+            },
+            want: map[string]interface{}{
+                "Number": responses[1].Number,
+                "Str": responses[1].Str,
+            },
+            wantErr: nil,
+            wantFetchErr: nil,
+        },
+
+    }
+    for _, tc := range cases {
+        result, got := GetSectionCommunicator(tc.section)
+        if (tc.wantErr == nil) && (tc.wantErr != got) {
+            t.Errorf("want %v, got %v", tc.wantErr, got)
+        } else if  (tc.want == nil) && (!result.Configured()) {
+    				t.Errorf("want %v, got %v, res %v", true, result.Configured(), result)
+        } else {
+            if !errors.Is(got, tc.wantErr) {
+                t.Errorf("want %v, got %v, res %v", tc.wantErr, got, result)
+            }
+        }
+        ctx, cancel := context.WithCancel(context.Background())
+        defer cancel() // cancel when we are getting the kill signal or exit
+        sent_params:=map[string]interface{}{
+            "a":"a",
+        }
+        if err:=result.Configure(tc.params); err != nil {
+            t.Errorf("want %v, got %v, results %v", nil, err, result)
+        }
+        c, _:= result.(*RestCommunicator)
+
+        if len(c.url) < 1 {
+            t.Errorf("want url len, got %v",  result)
+
+        }
+
+        ret, getFetchErr:= result.Fetch(ctx,sent_params)
+        if (tc.wantFetchErr == nil) && (tc.wantFetchErr != getFetchErr) {
+            t.Errorf("want %v, got %v", tc.wantFetchErr, getFetchErr)
+        // Be Aware that Keys are ToLower.
+        } else if (tc.wantFetchErr == nil) && (ret[0]["str"] !=  tc.want["Str"] ) {
+            t.Errorf("want %v, got %v", tc.want["Str"], ret[0]["str"])
+        } else {
+            if !errors.Is(getFetchErr, tc.wantFetchErr) {
+                t.Errorf("want %v, got %v, res %v", tc.wantFetchErr, getFetchErr, result)
+            }
+        }
+        if len(globalGot) <1 {
+            t.Errorf("want len > 0 , got %v, send params %v", globalGot, sent_params)
+        }
+
+    }
 }
