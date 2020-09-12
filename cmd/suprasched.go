f80b2eba4bf1c2e240f0fc1c83fd16f6c0e969d0
Valeriy Solovyov <weldpua2008@gmail.com>
2020-09-12 15:50:37 +0300

    [bugfix] All commands include extra code

--- cmd/suprasched.go
+++ cmd/suprasched.go
@@ -134,6 +134,35 @@ var rootCmd = &cobra.Command{
 		} else {
 			close(jobs)
 		}
+        // ctx, cancel := context.WithCancel(context.Background())
+        // defer cancel()
+        //
+        // if false {
+        // 	cl := cluster.NewDescribeEMR()
+        // 	params := make(map[string]interface{})
+        // 	params["ClusterID"] = "j-3JTIEH8MDWQ21"
+        // 	params["aws_profile"] = "bi-use1"
+        // 	params["ctx"] = ctx
+        //
+        // 	clusterId, _ := cl.DescribeCluster(params)
+        // 	log.Infof("%s", clusterId)
+        //
+        // }
+
+        // f, _ := cluster.NewFetchClustersHttp()
+        // // params :=make(map[string]interface{})
+        //
+        // cl, err := f.Fetch()
+        // if err == nil {
+        // 	for _, v := range cl {
+        // 		log.Infof("%v", v)
+        // 	}
+        //
+        // } else {
+        // 	log.Infof("%v, %v", cl, err)
+        //
+        // }
+
 
 		//
 		// for w := 1; w <= numWorkers; w++ {
--- main.go
+++ main.go
@@ -7,44 +7,14 @@
 package main
 
 import (
-	"context"
 	"github.com/sirupsen/logrus"
-	"github.com/weldpua2008/suprasched/cluster"
 	"github.com/weldpua2008/suprasched/cmd"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	log := logrus.WithFields(logrus.Fields{"package": "main"})
-
-	if false {
-		cl := cluster.NewDescribeEMR()
-		params := make(map[string]interface{})
-		params["ClusterID"] = "j-3JTIEH8MDWQ21"
-		params["aws_profile"] = "bi-use1"
-		params["ctx"] = ctx
-
-		clusterId, _ := cl.DescribeCluster(params)
-		log.Infof("%s", clusterId)
-
-	}
 
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
-	}
-	f, _ := cluster.NewFetchClustersHttp()
-	// params :=make(map[string]interface{})
-
-	cl, err := f.Fetch()
-	if err == nil {
-		for _, v := range cl {
-			log.Infof("%v", v)
-		}
-
-	} else {
-		log.Infof("%v, %v", cl, err)
-
 	}
 }
