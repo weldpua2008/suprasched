3af018a5804706cff60166f9a15cf54991381038
BarLisWix <87968265+BarLisWix@users.noreply.github.com>
2021-08-04 10:03:29 +0300

    Modify all functions (#8)
    Modify all functions, example function for working with cmd

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -9,73 +9,55 @@ import (
 	"time"
 )
 
-type TempObj struct {
-	Name        string
-	Age         int
-	Designation string
-	Salary      int
-}
-
 var (
 	dialTimeout    = 2 * time.Second
 	requestTimeout = 10 * time.Second
 )
 
-func EtcdConnection() {
+func KV_getExample(){
 	// The etcd client object is instantiated, configured with the dial time and the endpoint to the local etcd server
-	KVClient, err := clientv3.New(clientv3.Config{
+	client, err := clientv3.New(clientv3.Config{
 		DialTimeout: dialTimeout,
 		Endpoints:   []string{"127.0.0.1:2379"},
 	})
+
 	if err != nil {
 		log.Fatalf("Cannot start etcd client, got %s", err)
 		return
 	}
-	fmt.Println("connect success")
 
 	// The defer call is guaranteed to be used at the end of the function and ensures all etcd resources are released
-	defer KVClient.Close()
+	defer client.Close()
 
 	// context = Go feature that allows code across a goroutine to access shared information in a safe manner and cancel operations
-	//ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
-	//// releases resources if slowOperation completes before timeout elapses
-	//defer cancel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
 
 	// NewKV wraps a KV instance so that all requests are prefixed with a given string.
-	//kv := clientv3.NewKV(KVClient)
-}
+	kvClient := clientv3.NewKV(client)
 
-func GetValue(ctx context.Context, kv clientv3.KV, key string) {
-	// returns a response object with a header that contains the revision and a "Kvs" (key values) field, which is a slice of key-value pairs.
 
-	opts := []clientv3.OpOption{
-		//clientv3.WithPrefix(),
-		clientv3.WithSort(clientv3.SortByKey, clientv3.SortAscend),
-	}
+	// releases resources if slowOperation completes before timeout elapses
+	defer cancel()
 
-	gr, err := kv.Get(ctx, key, opts...)
+	kvClient.Delete(ctx, "", clientv3.WithPrefix())
+
+	kvPut, err := kvClient.Put(context.TODO(), "foo", "bar")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Put Function: Cannot put key & value, got %s",err)
 	}
 
-	for _, item := range gr.Kvs {
-		fmt.Println("Value:", string(item.Key), ",Value:", string(item.Value), ",Revision:", strconv.FormatInt(gr.Header.Revision, 10))
-	}
-}
-
-func GetPastValueFromRevision(ctx context.Context, kv clientv3.KV, key string, rev int64) {
-	// WithRev() returns the historical version of the value for the target key, but the header's revision will always contain the current revision!.
-	gr, _ := kv.Get(ctx, key, clientv3.WithRev(rev))
-	fmt.Println("Historical version of the value:", string(gr.Kvs[0].Value), ",Past revision:", strconv.FormatInt(rev, 10), ",Revision: ", strconv.FormatInt(gr.Header.Revision, 10))
-}
-
-func InsertsSingleValue(ctx context.Context, kv clientv3.KV, key string, val string) {
-	// Inserts a key value pair "key", "value" via the Put() method. the keys and values must be strings!
-	pr, err := kv.Put(ctx, key, val)
-	if err != nil {
-		log.Fatalf("Cannot put key/value, got %s", err)
-		return
-	}
-	rev := pr.Header.Revision
+	rev := kvPut.Header.Revision
 	fmt.Println("Revision:", rev)
+
+	resp, err := kvClient.Get(ctx, "foo")
+
+	if err != nil {
+		log.Fatalf("Get Function: Cannot get value, got %s",err)
+	}
+
+	for _, ev := range resp.Kvs {
+		fmt.Println("Value:", string(ev.Key), ",Value:" ,string(ev.Value), ",Revision:", rev)
+	}
 }
+
