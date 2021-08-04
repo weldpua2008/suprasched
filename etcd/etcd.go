ba37521bafce5ac24663ce6064b370ece4a9e51f
BarLisWix <87968265+BarLisWix@users.noreply.github.com>
2021-08-04 14:28:47 +0300

    Bar branch (#11)
    * Modify all functions
    
    Modify all functions, example function for working with cmd
    
    * get function whit receiving external values
    
    Add a sample get function whit receiving external values
    
    * add flag parse
    
    * Automated changes
    
    * writing to a yaml file
    
    Add a get function with writing to a yaml file
    
    Co-authored-by: Valeriy Soloviov <weldpua2008@users.noreply.github.com>

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -5,17 +5,26 @@ import (
 	"flag"
 	"fmt"
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"io/ioutil"
 	"log"
+
+	"gopkg.in/yaml.v2"
+
 	"time"
 )
+
+type MyKV struct {
+	Key string
+	Value  string
+	Revision int64
+}
 
 var (
 	dialTimeout    = 2 * time.Second
 	requestTimeout = 10 * time.Second
 )
 
-
-func KV_getExample(){ // Get function for example, without accepting external values
+func KV_getExample() { // Get function for example, without accepting external values
 
 	// The etcd client object is instantiated, configured with the dial time and the endpoint to the local etcd server
 	client, err := clientv3.New(clientv3.Config{
@@ -45,7 +54,7 @@ func KV_getExample(){ // Get function for example, without accepting external va
 
 	kvPut, err := kvClient.Put(context.TODO(), "foo", "bar")
 	if err != nil {
-		log.Fatalf("Put Function: Cannot put key & value, got %s",err)
+		log.Fatalf("Put Function: Cannot put key & value, got %s", err)
 	}
 
 	rev := kvPut.Header.Revision
@@ -54,17 +63,71 @@ func KV_getExample(){ // Get function for example, without accepting external va
 	resp, err := kvClient.Get(ctx, "foo")
 
 	if err != nil {
-		log.Fatalf("Get Function: Cannot get value, got %s",err)
+		log.Fatalf("Get Function: Cannot get value, got %s", err)
 	}
 
 	for _, ev := range resp.Kvs {
-		fmt.Println("Key:", string(ev.Key), ",Value:" ,string(ev.Value), ",Revision:", rev)
+		fmt.Println("Key:", string(ev.Key), ",Value:", string(ev.Value), ",Revision:", rev)
 	}
 }
 
-func KV_getExampleWithFlag(){ // Get function for example, with external values
+func KV_getExampleWithFlag() { // Get function for example, with external values
 
 	flagKey := flag.String("key", "tempKeyValue", "key for Get KeyValue function in etcd")
+
+	flag.Parse()
+
+	// The etcd client object is instantiated, configured with the dial time and the endpoint to the local etcd server
+	client, err := clientv3.New(clientv3.Config{
+		DialTimeout: dialTimeout,
+		Endpoints:   []string{"127.0.0.1:2379"},
+	})
+
+	if err != nil {
+		log.Fatalf("Cannot start etcd client, got %s", err)
+		return
+	}
+
+	// The defer call is guaranteed to be used at the end of the function and ensures all etcd resources are released
+	defer client.Close()
+
+	// context = Go feature that allows code across a goroutine to access shared information in a safe manner and cancel operations
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+
+	// NewKV wraps a KV instance so that all requests are prefixed with a given string.
+	kvClient := clientv3.NewKV(client)
+
+	// releases resources if slowOperation completes before timeout elapses
+	defer cancel()
+
+	kvClient.Delete(ctx, "", clientv3.WithPrefix())
+
+	myTime := time.Now().String()
+	kvPut, err := kvClient.Put(context.TODO(), *flagKey, myTime)
+	if err != nil {
+		log.Fatalf("Put Function: Cannot put key & value, got %s", err)
+	}
+
+	rev := kvPut.Header.Revision
+	fmt.Println("Revision:", rev)
+
+	resp, err := kvClient.Get(ctx, *flagKey)
+
+	if err != nil {
+		log.Fatalf("Get Function: Cannot get value, got %s", err)
+	}
+
+	for _, ev := range resp.Kvs {
+		fmt.Println("Key:", string(ev.Key), ",Value:", string(ev.Value), ",Revision:", rev)
+	}
+
+}
+
+func KV_getExampleWithWriteToYaml(){ // Get function for example, write to yaml with external values
+
+	flagKey := flag.String("key", "tempKeyValue", "key for Get KeyValue function in etcd")
+
+	flag.Parse()
 
 	// The etcd client object is instantiated, configured with the dial time and the endpoint to the local etcd server
 	client, err := clientv3.New(clientv3.Config{
@@ -108,6 +171,17 @@ func KV_getExampleWithFlag(){ // Get function for example, with external values
 
 	for _, ev := range resp.Kvs {
 		fmt.Println("Key:", string(ev.Key), ",Value:" ,string(ev.Value), ",Revision:", rev)
+		myKV := map[string]MyKV{"kv " + myTime: {string(ev.Key), string(ev.Value), rev}}
+		data, err := yaml.Marshal(&myKV)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err2 := ioutil.WriteFile("etcdConfigFile.yaml", data, 0222)
+		if err2 != nil {
+
+			log.Fatal(err2)
+		}
+
+		fmt.Println("data written")
 	}
 }
-
